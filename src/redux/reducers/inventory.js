4ac879ef0a69aa2ea00f058d28f45303cbac15b8
Dhudot Yukit <hgyukit@up.edu.ph>
2024-05-16 16:58:24 +0800

    Change initial states from {} to [].

--- src/redux/reducers/inventory.js
+++ src/redux/reducers/inventory.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  inventories: {},
+  inventories: [],
 };
 
 export const inventorySlice = createSlice({
--- src/redux/reducers/sales.js
+++ src/redux/reducers/sales.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  sales: {},
+  sales: [],
 };
 
 export const salesSlice = createSlice({
