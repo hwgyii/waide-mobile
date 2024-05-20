14b37de42ecb4114acd3e37a6b1134fa53b01f3f
Dhudot Yukit <hgyukit@up.edu.ph>
2024-05-21 01:27:55 +0800

    remove Description label when there's no description for the receipt.

--- src/components/CRUD/Receipts/ReceiptsModal.jsx
+++ src/components/CRUD/Receipts/ReceiptsModal.jsx
@@ -66,7 +66,7 @@ export default function ReceiptsModal({ receipt, index }) {
                 }}
               >
                 <Text>Receipt ID: {receipt._id.slice(10)}</Text>
-                <Text>Description: {receipt.description}</Text>
+                {receipt.description ? <Text>Description: {receipt.description}</Text> : null}
                 <Text>{dayjs(receipt.createdAt).add(8, "hours").format("MMM DD, YYYY hh:mm A")}</Text>
                 <Divider bgColor="black" marginTop={10}/>
                 {
